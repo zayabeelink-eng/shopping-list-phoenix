f4ae01a779b69f1bd45efccd8dd7fa54c09b8a9b
Lara <lara@Laras-MacBook-Air.local>
2026-06-27 23:08:08 +1000

    fix invalid regex E option in dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -60,12 +60,12 @@ config :shopping_list, ShoppingListWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/shopping_list_web/router\.ex$"E,
-      ~r"lib/shopping_list_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/shopping_list_web/router\.ex$",
+      ~r"lib/shopping_list_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
